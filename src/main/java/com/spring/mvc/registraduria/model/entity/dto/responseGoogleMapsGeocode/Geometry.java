8a8d59e771a2b0c62afe6c05a5531327cf25b3de
Christian <christian_kazuma@hotmail.com>
2020-08-20 11:22:33 -0500

    Se crea el dto para el servicio que se va a consumir de google maps y adicionalmente se deja funcionando el google maps con la key de la api

---
+++ src/main/java/com/spring/mvc/registraduria/model/entity/dto/responseGoogleMapsGeocode/Geometry.java
@@ -0,0 +1,45 @@
+package com.spring.mvc.registraduria.model.entity.dto.responseGoogleMapsGeocode;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * @author User
+ */
+public class Geometry {
+
+    @SerializedName("location")
+    @Expose
+    private Location location;
+    @SerializedName("location_type")
+    @Expose
+    private String locationType;
+    @SerializedName("viewport")
+    @Expose
+    private Viewport viewport;
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public String getLocationType() {
+        return locationType;
+    }
+
+    public void setLocationType(String locationType) {
+        this.locationType = locationType;
+    }
+
+    public Viewport getViewport() {
+        return viewport;
+    }
+
+    public void setViewport(Viewport viewport) {
+        this.viewport = viewport;
+    }
+
+}
