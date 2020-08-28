06b40bc1bb0bced3dd0d4dc2609957cabfeabf68
Christian <christian_kazuma@hotmail.com>
2020-08-28 10:59:01 -0500

    Se actualiza el controlador para obtener la informacion mediante un servicio de la consulta de una persona

--- src/main/java/com/spring/mvc/registraduria/controllers/HomeRestController.java
+++ src/main/java/com/spring/mvc/registraduria/controllers/HomeRestController.java
@@ -2,12 +2,15 @@ package com.spring.mvc.registraduria.controllers;
 
 import com.spring.mvc.registraduria.model.entity.*;
 import com.spring.mvc.registraduria.model.entity.dto.ConsultaDto;
+import com.spring.mvc.registraduria.model.entity.dto.responseGoogleMapsGeocode.Geocode;
+import com.spring.mvc.registraduria.model.entity.dto.responseGoogleMapsGeocode.Result;
 import com.spring.mvc.registraduria.model.entity.service.IPersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -49,9 +52,15 @@ public class HomeRestController {
                 persona.getJurado().equals("No es Jurado");
             }
             consultaDto.setMesaVoto(tablaMesa.getMesa());
-            consultaDto.setLugarVoto(tablaLugarVoto.getDireccionVoto());
+            consultaDto.setLugarVoto(tablaLugarVoto.getNombreLugar());
             consultaDto.setCiudadVoto(tablaCiudad.getCiudad());
             consultaDto.setDepartamentoVoto(tablaDepartamento.getDepartamento());
+            RestTemplate restTemplate = new RestTemplate();
+            String fooResourceUrl = "https://maps.googleapis.com/maps/api/geocode/json?address="+tablaLugarVoto.getNombreLugar()+", "+consultaDto.getCiudadVoto()+"&key=AIzaSyBP9BzaVQMLVAX5HaJOcOvYVkeoTMmv0CQ";
+            Geocode geocode=restTemplate.getForObject(fooResourceUrl, Geocode.class);
+            Result result=geocode.getResults().get(0);
+            consultaDto.setLat(result.getGeometry().getViewport().getNortheast().getLat());
+            consultaDto.setLng(result.getGeometry().getViewport().getNortheast().getLng());
             return consultaDto;
         }
     }
